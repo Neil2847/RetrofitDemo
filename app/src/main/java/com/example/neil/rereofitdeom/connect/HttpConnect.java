57ce193367a7c19c94b86d7d647194761e0d3227
Neil <neil@creatidea.com.tw>
2018-01-31 10:22:33 +0800

    [更新] 封裝完成

--- app/src/main/java/com/example/neil/rereofitdeom/connect/HttpConnect.java
+++ app/src/main/java/com/example/neil/rereofitdeom/connect/HttpConnect.java
@@ -1,20 +1,31 @@
 package com.example.neil.rereofitdeom.connect;
 
+import android.app.Application;
 import android.content.Context;
+import android.text.TextUtils;
 
 import com.example.neil.rereofitdeom.model.Museum;
+import com.example.neil.rereofitdeom.util.MyApplication;
 import com.example.neil.rereofitdeom.util.MyObserver;
 import com.example.neil.rereofitdeom.util.ObserverOnNextListener;
 
 import io.reactivex.Observable;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observer;
 import io.reactivex.schedulers.Schedulers;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 
+import okhttp3.Cache;
+import okhttp3.CacheControl;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -26,7 +37,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class HttpConnect {
 
     // --------------------------------------------------------
-    private final int _TIME_OUT = 10;
+    private static final long CACHE_STALE_SEC = 60 * 60 * 24 * 2;
+
+    private static final int _READ_TIME_OUT = 5;
+    private static final int _CONNECT_TIME_OUT = 5;
 
     private Retrofit retrofit;
     private APIService service;
@@ -49,8 +63,33 @@ public class HttpConnect {
     // --------------------------------------------------------
     private void init() {
 
-        OkHttpClient client = new OkHttpClient.Builder().build();
-        client.newBuilder().connectTimeout(_TIME_OUT, TimeUnit.MINUTES);
+        HttpLoggingInterceptor logInterceptor = new HttpLoggingInterceptor();
+        logInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        // set cache path
+//        File cacheFile = new File(MyApplication.getContext().getCacheDir(), "cache");
+//        // set cache 100 MB
+//        Cache cache = new Cache(cacheFile, 1024 * 1024 * 100);
+        // add header message.
+//        Interceptor headerInterceptor = new Interceptor() {
+//            @Override
+//            public Response intercept(Chain chain) throws IOException {
+//                Request build = chain.request().newBuilder()
+//                        .addHeader("Content-Type", "application/json")
+//                        .build();
+//                return chain.proceed(build);
+//            }
+//        };
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(_CONNECT_TIME_OUT, TimeUnit.MINUTES)
+                .readTimeout(_READ_TIME_OUT, TimeUnit.MINUTES)
+//                .addInterceptor(mRewriteCacheControlInterceptor)
+//                .addNetworkInterceptor(mRewriteCacheControlInterceptor)
+//                .addInterceptor(headerInterceptor)
+                .addInterceptor(logInterceptor)
+//                .cache(cache)
+                .build();
 
         retrofit = new Retrofit.Builder()
                 .baseUrl(Config.WEB)
@@ -63,13 +102,43 @@ public class HttpConnect {
     }
 
     // --------------------------------------------------------
+    /**
+     * Dangerous interceptor that rewrites the server's cache-control header.
+     */
+//    private final Interceptor mRewriteCacheControlInterceptor = new Interceptor() {
+//        @Override
+//        public Response intercept(Chain chain) throws IOException {
+//            Request request = chain.request();
+//            String cacheControl = request.cacheControl().toString();
+//            if (!NetWorkUtils.isNetConnected(MyApplication.getContext())) {
+//                request = request.newBuilder()
+//                        .cacheControl(TextUtils.isEmpty(cacheControl) ? CacheControl
+//                                .FORCE_NETWORK : CacheControl.FORCE_CACHE)
+//                        .build();
+//            }
+//            Response originalResponse = chain.proceed(request);
+//            if (NetWorkUtils.isNetConnected(MyApplication.getContext())) {
+//                return originalResponse.newBuilder()
+//                        .header("Cache-Control", cacheControl)
+//                        .removeHeader("Pragma")
+//                        .build();
+//            } else {
+//                return originalResponse.newBuilder()
+//                        .header("Cache-Control", "public, only-if-cached, max-stale=" +
+//                                CACHE_STALE_SEC)
+//                        .removeHeader("Pragma")
+//                        .build();
+//            }
+//        }
+//    };
+
+    // --------------------------------------------------------
     public void getMuseumList(Context context, ObserverOnNextListener<Museum> listener) {
         addObserver(service.loadMuseumList(1, Config.NMTH), new MyObserver<Museum>(context, listener));
     }
 
     // --------------------------------------------------------
     private <T> void addObserver(Observable<T> data, Observer<T> observer) {
-
         data.subscribeOn(Schedulers.io())
                 .unsubscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
--- app/src/main/java/com/example/neil/rereofitdeom/util/MyApplication.java
+++ app/src/main/java/com/example/neil/rereofitdeom/util/MyApplication.java
@@ -1,6 +1,7 @@
 package com.example.neil.rereofitdeom.util;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
@@ -10,11 +11,19 @@ import com.orhanobut.logger.Logger;
  * Created by neil on 2018/1/25.
  */
 
-public class MyApplication extends Application{
+public class MyApplication extends Application {
 
+    // --------------------------------------------------------
     @Override
     public void onCreate() {
         super.onCreate();
         Logger.addLogAdapter(new AndroidLogAdapter());
     }
+
+    // --------------------------------------------------------
+//    public static Context getContext() {
+//        return getContext();
+//    }
+
+    // --------------------------------------------------------
 }
